37ca34431cd2a120df2a5794c68c89211d9a4038
PabloP05 <pablopardorodriguez.guadalupe@alumnado.fundacionloyola.net>
2026-01-08 13:31:19 +0100

    app terminada

--- app.js
+++ app.js
@@ -2,16 +2,31 @@ let formulario = document.getElementById('formulario');
 let nombre;
 
 let foto = document.getElementById('fotoPokemon'); 
-let nombrePokemon = document.getElementById('nombrePokemon')
-let divPokemon = document.getElementById('mostrar')
+let nombrePokemon = document.getElementById('nombrePokemon');
+let divPokemon = document.getElementById('mostrar');
+
+let coleccion = document.getElementById('coleccion');
+
+let botonAgregar = document.getElementById('botonAgregar');
+
 
 formulario.addEventListener('submit',(event)=>{
     event.preventDefault();
     nombre = document.getElementById('nobrePokemon').value;
+    
     nombre.toLowerCase();
    buscarPokemon(nombre);
+
      
 });
+
+
+botonAgregar.addEventListener('click',()=>{
+    //console.log(foto.src);
+    nombre = document.getElementById('nobrePokemon').value;
+    cargarColeccion(nombre,foto.src);
+});
+
 
 
 async function buscarPokemon(pokemon){
@@ -20,10 +35,18 @@ async function buscarPokemon(pokemon){
     console.log(datosParseados);
     foto.src = datosParseados.sprites.front_default
     nombrePokemon.innerText = pokemon
+}
 
-    for (const element of datosParseados.abilities) {
-        let p = document.createElement('p');
-        p.innerText = element.ability;
-        divPokemon.appendChild(p);
-    }
+function cargarColeccion(nombrePokemon,imagen){
+    let div = document.createElement('div');
+    let nombreAgregado = document.createElement('h3');
+    let imagenAgregado = document.createElement('img');
+
+    nombreAgregado.innerText = nombrePokemon;
+    imagenAgregado.src = imagen;
+    div.appendChild(nombreAgregado);
+    div.appendChild(imagenAgregado);
+
+    coleccion.appendChild(div);
+
 }
